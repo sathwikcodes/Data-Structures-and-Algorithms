890415658b222e7d2f54c9a9f68a4a608dd829eb
Sathwik <bnvsathwik75@gmail.com>
2024-04-21 23:00:48 +0530

    LL

--- Striver/LinkedList/LL.cpp
+++ Striver/LinkedList/LL.cpp
@@ -35,6 +35,30 @@ Node *convertArr2LL(vector<int> &arr)
     return head;
 }
 
+Node *removeHeads(Node *head)
+{
+    if (head == NULL)
+        return head;
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+
+Node *removeTails(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+        return NULL;
+    Node *temp = head;
+    while (temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
+    return head;
+}
+
 int lengthofLL(Node *head)
 {
     int cnt = 0;
@@ -47,21 +71,76 @@ int lengthofLL(Node *head)
     return cnt;
 }
 
-int checkIfPresent(Node* head, int val){
+int checkIfPresent(Node *head, int val)
+{
     Node *temp = head;
-    while(temp){
-        if(temp->data == val)
+    while (temp)
+    {
+        if (temp->data == val)
             return 1;
         temp = temp->next;
     }
     return 0;
 }
 
+Node *removeK(Node *head, int k)
+{
+    if (head == NULL)
+        return head;
+    if (k == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    int cnt = 0;
+    Node *temp = head;
+    Node *prev = NULL;
+    while (temp != NULL)
+    {
+        cnt++;
+        if (cnt == k)
+        {
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+Node* removeElement(Node* head, int el){
+    if(head == NULL)
+        return head;
+    if(head->data == el){
+        Node *temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    Node *temp = head;
+    Node *prev = NULL;
+    while(temp != NULL){
+        if(temp->data == el){
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
 int main()
 {
     vector<int> arr = {2, 5, 8, 7};
     Node *head = convertArr2LL(arr);
-    Node *temp = head;
+    // print(head);
+    Node *temp = removeK(head, 3);
     while (temp)
     {
         cout << temp->data << " ";
