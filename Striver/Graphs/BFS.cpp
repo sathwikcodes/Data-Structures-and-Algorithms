ce5a44039c2075a30378a9ff77948ef313e5428f
Sathwik Batta <bnvsathwik75@gmail.com>
2024-06-20 23:49:22 +0530

    Graphs

---
+++ Striver/Graphs/BFS.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[])
+    {
+        // Code here
+
+        int vis[n] = {0};
+        vis[0] = 1;
+        queue<int> q;
+        q.push(0);
+        vector<int> bfs;
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            bfs.push(node);
+
+            for (auto it : adj[node])
+            {
+                if (!vis[it])
+                {
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};
+
